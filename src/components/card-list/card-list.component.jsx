b9a584d63eec94e0b89f29cff9fca91c86129c7a
UdithaIshan <uditha.ishan34@gmail.com>
2020-10-25 14:23:07 +0530

    updated

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './card-list.styles.css';
+import {Card} from '../card/card.component'
 
 export const CardList = (props) => {
     return <div className="card-list">
         {props.avatars.map((avatar) => (
-          <h1 key={avatar.key}>{avatar.name}</h1>
+          <Card key="avatars.key" avatar={avatar}/> 
         ))}
         </div>;
 }
