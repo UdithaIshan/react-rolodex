823e6b23643d2074094c95a4ca163437235725fa
UdithaIshan <uditha.ishan34@gmail.com>
2020-10-25 16:41:39 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -24,9 +25,7 @@ class App extends Component {
 
   return(
     <div className="App">
-      <input type="search" placeholder="Search" onChange={(e) => {
-        this.setState({searchKey:e.target.value})
-      }}/>
+      <SearchBox placeholder="Search" handleChange={(e) => {this.setState({searchKey:e.target.value}) }}/>
       <CardList avatars={filterdAvatars}/>
     </div>
   );
