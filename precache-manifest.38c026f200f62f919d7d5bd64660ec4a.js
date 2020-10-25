b5c3f33c38d73ccbe2d8475027809095dc49c72d
UdithaIshan <uditha.ishan34@gmail.com>
2020-10-25 22:01:34 +0530

    Updates

---
+++ precache-manifest.38c026f200f62f919d7d5bd64660ec4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d615224950ba46906745c38a506718b9",
+    "url": "/udithaishan.github.io/react-rolodex/index.html"
+  },
+  {
+    "revision": "1b5e4990769f948f74ad",
+    "url": "/udithaishan.github.io/react-rolodex/static/css/main.62c736b5.chunk.css"
+  },
+  {
+    "revision": "51469f9bb0d0c86ea789",
+    "url": "/udithaishan.github.io/react-rolodex/static/js/2.d3a08ced.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/udithaishan.github.io/react-rolodex/static/js/2.d3a08ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5e4990769f948f74ad",
+    "url": "/udithaishan.github.io/react-rolodex/static/js/main.d70badf4.chunk.js"
+  },
+  {
+    "revision": "4b16ccf5035e93cb0617",
+    "url": "/udithaishan.github.io/react-rolodex/static/js/runtime-main.30565153.js"
+  }
+]);
